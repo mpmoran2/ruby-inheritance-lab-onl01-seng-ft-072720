a6a5f0b77d88037f1494584767962d04df774bd1
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 20:51:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,4 @@
 class Student < User 
-  attr_accessor :knowledge
-  
-  def initialize 
-    @knowledge = []
-  end 
-  
-  def learn(string)
-    @knowledge << string 
-  end 
-  
-  def knowledge
-    @knowledge
-  end 
+
   
 end
